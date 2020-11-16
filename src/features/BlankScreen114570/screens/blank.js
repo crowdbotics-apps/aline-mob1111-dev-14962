6af35b75387618bef6b91c5fd714d63e32bd510b
AlineTavares <aline@crowdbotics.com>
2020-11-16 15:12:25 -0500

    Update screen

--- src/features/BlankScreen114570/screens/blank.js
+++ src/features/BlankScreen114570/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,53 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { Switch_7: true }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text style={styles.Text_3}>
+        Select this text to return to blank screen.
+      </Text>
+      <Icon name="bitbucket" />
+      <Switch
+        activeColor="#42ff58"
+        inactiveColor="#FF0000"
+        style={styles.Switch_7}
+        value={this.state.Switch_7}
+        onValueChange={nextChecked => this.setState({ Switch_7: nextChecked })}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  Text_3: {
+    fontSize: 30,
+    color: "#D50700",
+    textDecorationLine: "none",
+    fontFamily: "Merriweather",
+    fontStyle: 'italic',
+    textTransform: "lowercase",
+  },
+  Icon_5: {},
+  Switch_7: { alignSelf: "flex-start" }
+})
